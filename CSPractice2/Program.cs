e95198a36ab9cf3ec6344de7864d0c28083318f5
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 08:55:11 +0900

    #6 23-7. 섀도잉

---
+++ CSPractice2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPractice2
+{
+    internal class Program
+    {
+        int number = 10;
+
+        static void Main(String[] args)
+        {
+            // #6 23-7. 섀도잉
+            int number = 20;
+            Console.WriteLine(number); // 20
+        }
+    }
+}
