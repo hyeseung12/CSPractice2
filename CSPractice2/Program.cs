74734fb90830835d86282b923f4f924523baec1e
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 09:07:37 +0900

    #6 23-7. 하이딩

--- CSPractice2/Program.cs
+++ CSPractice2/Program.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace CSPractice2
 {
+    class Parent
+    {
+        public int variable = 273;
+        public void Method()
+        {
+            Console.WriteLine("부모 메서드");
+        }
+    }
+
+    class Child : Parent
+    {
+        public new string variable = "하이딩";
+        public new void Method()
+        {
+            Console.WriteLine("자식 메서드");
+        }
+    }
+
     internal class Program
     {
         int number = 10;
@@ -15,6 +33,18 @@ namespace CSPractice2
             // #6 23-7. 섀도잉
             int number = 20;
             Console.WriteLine(number); // 20
+
+            // #6 23-7. 하이딩
+            Child child = new Child();
+            Console.WriteLine(child.variable); // 출력: 하이딩
+            child.Method(); // 출력: 자식 메서드
+            Console.WriteLine(((Parent)child).variable); // 출력: 273
+            ((Parent)child).Method(); // 출력: 부모 메서드
+            Parent p = child;
+            Console.WriteLine(p.variable); // 출력: 273
+            p.Method(); // 출력: 부모 메서드
+            ((Child)p).Method(); // 출력: 자식 메서드
+
         }
     }
 }
